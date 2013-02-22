bfa6ebbfe1e53affbf75563508d82799e37095c9
lhuang89 <lhuang89@gmail.com>
2013-02-22 14:24:03 -0600

    Test for Egit fetch

--- ObjectComparison_Strategy/src/proj/Algorithm/QuickSort.java
+++ ObjectComparison_Strategy/src/proj/Algorithm/QuickSort.java
@@ -8,6 +8,7 @@ public class QuickSort<T> implements SortAlgorithm<T> {
 	@Override
 	public ArrayList<T> Sort(ArrayList<T> array, Comparator<T> comparator) {
 		// TODO Auto-generated method stub
+		// This is to test fetch of Egit
 		return null;
 	}
 
