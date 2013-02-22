f2c2c5d1e95c10f5ff472a813bd20c5986870ceb
lhuang89 <lhuang89@gmail.com>
2013-02-22 14:10:24 -0600

    Updated mergesort and test Egit

--- ObjectComparison_Strategy/src/proj/Algorithm/MergeSort.java
+++ ObjectComparison_Strategy/src/proj/Algorithm/MergeSort.java
@@ -7,6 +7,14 @@ public class MergeSort<T> implements SortAlgorithm<T> {
     @Override
 	public ArrayList<T> Sort(ArrayList<T> array,Comparator<T> comparator)
     {
+    	Split();
+    	Merge();
     	return null;
     }
+    
+    private void Split()
+    {}
+    
+    private void Merge()
+    {}
 }
